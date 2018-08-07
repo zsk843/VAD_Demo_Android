089c6c6788062038944fad2ab881238da713f02b
zsk843 <zsk843@icloud.com>
2018-08-07 11:12:24 +0800

    Finish. But not incremental std and means

--- app/src/main/java/com/example/yckj/vad_demo_android/LabelGeneration.java
+++
@@ -1,9 +0,0 @@
-package com.example.yckj.vad_demo_android;
-
-public abstract class LabelGeneration {
-    public abstract float[] Generate(float[] data);
-    public LabelGeneration(int[] input_dim) {
-    }
-    public abstract float[] GetInputCache();
-    public abstract int[] GetOutputDimension();
-}
